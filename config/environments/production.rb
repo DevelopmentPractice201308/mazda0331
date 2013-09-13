da979969a54ff123e54e6837bb34bb4b60a055e1
mazda <yuki_m-11b@stu.kbc.ac.jp>
2013-09-13 11:40:23 +0900

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,9 @@ SampleApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+# アプリケーションへのすべてのアクセスをSSL経由にし、
+  # Strict-Transport-Securityを使用し、
+  # secure cookiesを使用する。
+  config.force_ssl = true
+  
 end
